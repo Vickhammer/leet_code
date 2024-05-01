d95c3f30cd90a2206a4bc006687c699584eb69c7
Vickhammer <tvickhammer@gmail.com>
2024-04-30 20:21:30 -0700

    num 189 of top interview 150

---
+++ majority_element.rb
@@ -0,0 +1,29 @@
+# 169. Majority Element
+# Solved
+# Easy
+# Topics
+# Companies
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+# @param {Integer[]} nums
+# @return {Integer}
+# def majority_element(nums)
+#     nums_hash = Hash.new(0)
+
+#     nums.each do |n|
+#         nums_hash[n] += 1
+#     end
+
+#     nums.max_by{|v| nums_hash[v]}
+# end
+
+test = [1,2,3,4,5,6,7]
+
+def rotate(nums, k)
+        
+  p nums.rotate(-k)
+end
+
+rotate(test,3)
